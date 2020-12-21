b0d27af4690cb84a7a3e48f495061b08cbe65419
matth433 <kisiahmatthew@hotmail.com>
2020-12-21 16:53:08 -0600

    python-static-site-generator

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,7 +10,7 @@ class Parser:
         return extension in self.extentions
 
     def parse(self, path: Path,source: Path, dest: Path):
-    raise NotImplementedError
+        raise NotImplementedError
 
 def read(self,path):
     with open(path, "r") as file:
