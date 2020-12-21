07f340cc0622cb799a9f1502b86a874656a5edff
matth433 <kisiahmatthew@hotmail.com>
2020-12-21 17:04:26 -0600

    python-static-site-generator

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -12,18 +12,17 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-def read(self, path):
-    with open(path, "r") as file:
-        return file.read()
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
 
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
 
-def write(self, path, dest, content, ext=".html"):
-    full_path = dest / path.with_suffix(ext).name
-    with open(full_path, "w") as file:
-        file.write(content)
-
-def copy(slef, path, source, dest):
-    shutil.copy2(path, dest / path.relative_to(source))
+    def copy(slef, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     extentions = [".jpg", ".png", ".gif", ".css", ".html"]
