dd7078fa1994385bac087ff8c34091071fba509e
matth433 <kisiahmatthew@hotmail.com>
2020-12-21 17:43:46 -0600

    python-static-site-generator

--- ssg/content.py
+++ ssg/content.py
@@ -6,9 +6,10 @@ from yaml import load, FullLoader
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+
     @classmethod
     def load(cls, string):
-        _, fm, content = cls.__regex.split(ststring, 2)
+        _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
     def __init__(self, metadata, content):
@@ -35,7 +36,8 @@ class Content(Mapping):
 
     def __len__(self):
         return len(self.data)
-    def __reper__(self):
+
+    def __repr__(self):
         data = {}
         for key, value in self.data.items():
             if key != "content":
